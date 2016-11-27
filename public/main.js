096dd831749a63ef6beb87bd5194af5bfe9fbea3
Marko Haanranta <marko.haanranta@helsinki.fi>
2016-11-27 19:25:02 +0200

    pen.js for markdown

--- public/main.js
+++ public/main.js
@@ -4,3 +4,17 @@ const App = new Vue({
     msg: "hi there",
   }
 });
+
+const HuskEditorOptions = {
+  editor: document.getElementById('Editor'), // {DOM Element} [required]
+  class: 'pen', // {String} class of the editor,
+  debug: false, // {Boolean} false by default
+  textarea: '<textarea name="content"></textarea>', // fallback for old browsers
+  list: [
+    'blockquote', 'h2', 'h3', 'p', 'insertorderedlist', 'insertunorderedlist',
+    'indent', 'outdent', 'bold', 'italic', 'underline', 'createlink'
+  ], // editor menu list
+  linksInNewWindow: true // open hyperlinks in a new windows/tab
+}
+
+const HuskEditor = new Pen(HuskEditorOptions);
